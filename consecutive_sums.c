cdb29974b7ce45ddda9d4c2191d266ce625d26b9
Eun Cho <choeun8339@naver.com>
2020-10-31 12:25:04 +0900

    Initial Commit

---
+++ consecutive_sums.c
@@ -0,0 +1,14 @@
+/* In file consecutive_sums.c */
+
+#include <stdio.h>
+
+int main(void)
+{
+	int i = 1, sum = 0;
+	while (i <= 5) {
+		sum += i;
+		++i;
+	}
+	printf("sum = %d\n", sum);
+	return 0;
+}
